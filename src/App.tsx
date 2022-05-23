cda0b85451fe1834e065222da2a99c6be7d2df8c
sayil1 <sundaysayil4u@gmail.com>
2022-05-23 13:54:04 +0100

    make header menu responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Hero } from './components/hero';
-import { HeaderMenu } from './components/header_menu/header_meny';
+import { HeaderMenu } from './components/header_menu/header_menu';
 
 function App() {
   return (
