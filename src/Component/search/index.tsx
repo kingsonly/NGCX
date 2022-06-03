9a1befc25ed2d29f836b3e1c0929fc57b5a50ec6
sayil1 <sundaysayil4u@gmail.com>
2022-06-03 17:31:10 +0100

    add test for search component

--- src/Component/search/index.tsx
+++ src/Component/search/index.tsx
@@ -1,38 +1,46 @@
-import React, {FC} from 'react'
-import { FiSearch, FiChevronDown } from "react-icons/fi"
-import { DropDownMenu } from '..'
-
+import React, { FC } from "react";
+import { FiSearch, FiChevronDown } from "react-icons/fi";
+import { DropDownMenu } from "..";
 
 const items = [
-    {
-        label: "All Categories"
-    },
-    {
-        label: "Second item"
-    },
-    {
-        label: "Third item"
-    }
-]
+  {
+    label: "All Categories",
+  },
+  {
+    label: "Second item",
+  },
+  {
+    label: "Third item",
+  },
+];
 
-export const SearchInput:FC = () => {
-    return(
-        <div className='flex items-center justify-between w-full'>
-            <input placeholder='Search for items' className='outline-none px-2  w-full border-l placeholder:text-xs placeholder:text-gray-300'/>
-            <span className='px-4'><FiSearch color="lightgrey" size={20} /></span>
-        </div>
-    )
-}
+export const SearchInput: FC = () => {
+  return (
+    <div className="flex items-center justify-between w-full">
+      <input
+        data-testid="search-input"
+        placeholder="Search for items"
+        className="outline-none px-2  w-full border-l placeholder:text-xs placeholder:text-gray-300"
+      />
+      <span className="px-4">
+        <FiSearch color="lightgrey" size={20} />
+      </span>
+    </div>
+  );
+};
 
 const index = () => {
   return (
-    <div className='border-light-green border-2 rounded-md flex items-center w-full py-2'>
-        <DropDownMenu variant='searchDdm' items={items} label={items[0].label} icon={<FiChevronDown size={16}/>} />
-        <SearchInput />
+    <div className="border-light-green border-2 rounded-md flex items-center w-full py-2">
+      <DropDownMenu
+        variant="searchDdm"
+        items={items}
+        label={items[0].label}
+        icon={<FiChevronDown size={16} />}
+      />
+      <SearchInput />
     </div>
-  )
-}
+  );
+};
 
-
-
-export default index
+export default index;
